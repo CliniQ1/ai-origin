5f78553f5c5e6c700da03c707915eafd889b95a3
CliniQ1 <azer.ismailov.perm@gmail.com>
2026-01-19 00:17:41 +0500

    Include root router in API v1 router

--- src/app/api/v1/router.py
+++ src/app/api/v1/router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
-from app.api.v1.endpoints import health
+from app.api.v1.endpoints import health, root
 
 router = APIRouter(prefix="/api/v1")
 
+router.include_router(root.router)
 router.include_router(health.router)
