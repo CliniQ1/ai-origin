ee4b99c48353f6ce16a38da073d3cdff03cfde4f
CliniQ1 <azer.ismailov.perm@gmail.com>
2026-01-19 00:56:40 +0500

    Remove root endpoint from API router
    Removed root endpoint from router.

--- src/app/api/v1/router.py
+++ src/app/api/v1/router.py
@@ -1,9 +1,9 @@
+# src/app/api/v1/router.py
 from fastapi import APIRouter
-from .endpoints import auth, health, root
+from .endpoints import auth, health
 
 router = APIRouter()
 
-# Подключаем endpoints
+# Подключаем эндпоинты
 router.include_router(auth.router)
 router.include_router(health.router)
-router.include_router(root.router)
