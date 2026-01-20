d24c59fbfc412cceeffac1f87ef74ce677421e9c
CliniQ1 <azer.ismailov.perm@gmail.com>
2026-01-20 20:08:38 +0500

    Update and rename root.py to router.py

---
+++ src/app/api/v1/endpoints/router.py
@@ -0,0 +1,9 @@
+from fastapi import APIRouter
+
+from .health import router as health_router
+from .auth import router as auth_router
+
+router = APIRouter()
+
+router.include_router(health_router)
+router.include_router(auth_router)
