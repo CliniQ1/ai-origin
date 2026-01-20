8bb640ddd892430042c12b67414c5619b34bbd79
CliniQ1 <azer.ismailov.perm@gmail.com>
2026-01-20 20:09:26 +0500

    Fix import path for v1_router in main.py
    Updated import statement for v1_router to reflect new module structure.

--- src/app/main.py
+++ src/app/main.py
@@ -1,13 +1,10 @@
-# src/app/main.py
 from fastapi import FastAPI
-from app.api.v1 import router as v1_router
+from app.api.v1.endpoints.router import router as v1_router
 
 app = FastAPI()
 
-# Healthcheck (корневой /health)
 @app.get("/health")
 async def health():
     return {"status": "ok"}
 
-# Подключаем v1 router
 app.include_router(v1_router, prefix="/api/v1")
