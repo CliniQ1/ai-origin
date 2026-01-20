ae011709f32faf4d2d7f77c1ad713b74f21d9ae6
CliniQ1 <azer.ismailov.perm@gmail.com>
2026-01-20 23:26:03 +0500

    Update router.py

--- src/app/api/v1/router.py
+++ src/app/api/v1/router.py
@@ -1,8 +1,8 @@
+# src/app/api/v1/router.py
 from fastapi import APIRouter
-from app.api.v1.endpoints import auth, health, root
+from app.api.v1.endpoints import auth, root, health  # root уже создан
 
 api_router = APIRouter()
-
+api_router.include_router(root.router)
 api_router.include_router(auth.router)
 api_router.include_router(health.router)
-api_router.include_router(root.router)
