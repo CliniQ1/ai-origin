86ebce9f1587854a8e03c487c51dd9883547a071
CliniQ1 <azer.ismailov.perm@gmail.com>
2026-01-20 23:24:41 +0500

    Add AuthResponse model to auth.py

--- src/app/schemas/auth.py
+++ src/app/schemas/auth.py
@@ -1,5 +1,10 @@
+# src/app/schemas/auth.py
 from pydantic import BaseModel, EmailStr
 
 class LoginRequest(BaseModel):
     email: EmailStr
     password: str
+
+class AuthResponse(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
