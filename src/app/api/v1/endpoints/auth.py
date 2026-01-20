b3473a3537061e66cc7e3d480c9f093f195e9268
CliniQ1 <azer.ismailov.perm@gmail.com>
2026-01-20 23:38:16 +0500

    Update auth.py

--- src/app/api/v1/endpoints/auth.py
+++ src/app/api/v1/endpoints/auth.py
@@ -1,4 +1,3 @@
-# src/app/api/v1/endpoints/auth.py
 from fastapi import APIRouter, HTTPException, status
 from app.schemas.auth import LoginRequest, AuthResponse
 
@@ -7,14 +6,14 @@ router = APIRouter(
     tags=["auth"]
 )
 
-# Заглушка для проверки POST
 @router.post("/login", response_model=AuthResponse)
 async def login(data: LoginRequest):
-    # Временная логика: проверка email и password
-    if data.email == "test@example.com" and data.password == "123456":
+    # Тестовая логика авторизации
+    if data.email == "user@example.com" and data.password == "123456":
         return {"access_token": "fake_token_for_now", "token_type": "bearer"}
     
     raise HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Неверный email или пароль"
     )
+
